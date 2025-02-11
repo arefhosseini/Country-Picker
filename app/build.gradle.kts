206f8051db66f323b81e5311877ab230fa09ef80
arefhosseini <aref.hosseini@peaks.com>
2025-02-11 16:14:12 +0100

    Update SDK version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.arpitkatiyarprojects.countrypickerproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.arpitkatiyarprojects.countrypickerproject"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
